bc48b1cae8ce66ff4285ca86be19448374e01d89
scelosmano1 <scelosmano1@etf.unsa.ba>
2021-05-23 16:07:57 +0200

    unneeded wmc class removed

--- src/main/java/com/github/mauricioaniche/ck/CKVisitor.java
+++ src/main/java/com/github/mauricioaniche/ck/CKVisitor.java
@@ -1253,7 +1253,7 @@ public class CKVisitor extends ASTVisitor {
 	}
 
 	public void endVisit(IfStatement node) {
-		classes.peek().classLevelMetrics.stream().map(metric -> (CKASTVisitor) metric).forEach(ast -> ast.endVisit(node));
+  		classes.peek().classLevelMetrics.stream().map(metric -> (CKASTVisitor) metric).forEach(ast -> ast.endVisit(node));
 		if(!classes.peek().methods.isEmpty())
 			classes.peek().methods.peek().methodLevelMetrics.stream().map(metric -> (CKASTVisitor) metric).forEach(ast -> ast.endVisit(node));
 	}
